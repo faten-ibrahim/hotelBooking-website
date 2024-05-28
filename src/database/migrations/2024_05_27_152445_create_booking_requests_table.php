eefc97a4891788b4760a6bfa886a375f7dc29ec9
faten-ibrahim <faten_elssayad@yahoo.com>
2024-05-28 17:30:15 +0300

    add database schema

---
+++ src/database/migrations/2024_05_27_152445_create_booking_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_requests', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->tinyInteger('status');
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('room_id');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreign('room_id')->references('id')->on('rooms')
+                ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_requests');
+    }
+};
