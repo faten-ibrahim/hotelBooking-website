e0b943b265ff42b227aa641bcfb6f9c0ce135274
faten-ibrahim <faten_elssayad@yahoo.com>
2024-05-29 20:51:14 +0300

    only admin can list requests and approve or reject them

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\BookingRequestController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoomController;
 use App\Models\BookingRequest;
+use App\Models\Permission;
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,10 +22,13 @@ Route::group(['middleware' => 'auth'], function () {
     });
 
     Route::controller(BookingRequestController::class)->group(function () {
-        Route::get('/booking-requests', 'index')->name('bookingRequests.index');
-        Route::get('/booking-requests/edit', 'ApproveOrReject')->name('bookingRequests.ApproveOrReject');
-        Route::post('/booking-requests', 'store')->name('bookingRequests.store');
 
+        Route::group(['middleware' => ['can:'.Permission::MANAGE_DASHBOARD]], function () {
+            Route::get('/booking-requests', 'index')->name('bookingRequests.index');
+            Route::get('/booking-requests/edit', 'ApproveOrReject')->name('bookingRequests.ApproveOrReject');
+        });
+
+        Route::post('/booking-requests', 'store')->name('bookingRequests.store');
     });
 });
 require __DIR__ . '/auth.php';
